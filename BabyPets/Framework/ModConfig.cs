1185d9615ff2403477ce36874f0be5694f5804d5
FESH-pl <35628495+DelphinWave@users.noreply.github.com>
2024-03-24 20:59:48 +0100

    Add project files.

---
+++ BabyPets/Framework/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyPets.Framework
+{
+    public sealed class ModConfig
+    {
+        public int AdultPetAge { get; set; } = 28;
+    }
+}
